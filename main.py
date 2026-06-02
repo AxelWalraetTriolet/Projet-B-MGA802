f01d2f6200a4735f02e17483d039ef33410bbd77
axelw <axel.walraet-triolet.1@ens.etsmtl.ca>
2026-06-02 14:20:15 -0400

    Premier commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Projet B - MGA802 - Intégration numérique
+Paul SERRA, Naïs VIGROUX & Axel WALRAET-TRIOLET
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import timeit
+import time
+
+
